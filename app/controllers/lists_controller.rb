2c5f4b07f4a92ba9a229a016170a9598b75d9550
Victor Degrenier <victor-degrenier@hotmail.fr>
2025-02-27 16:53:14 +0100

    création des routes, models, controllers et views -- affichage de la lists -- affichages des bookmarks dans une list

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,15 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+puts "--------------------------------"
+puts "Cleaning actualy database..."
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
+puts "--------------------------------"
+puts "Creating database..."
+puts "--------------------------------"
+puts "Creating movies..."
+puts "--------------------------------"
+50.times do
+  movie = Movie.new(title: Faker::Movie.title, overview: Faker::Lorem.paragraph, poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: rand(0.0..10.0))
+  movie.save
+end
+puts "Finished! Created #{Movie.count} movies."
+puts "--------------------------------"
+
+puts "Creating list..."
+10.times do
+  list = List.new(name: Faker::Artist.name)
+  list.save
+end
+puts "Finished! Created #{List.count} lists."
+puts "--------------------------------"
+
+puts "Creating bookmarks..."
+40.times do
+  bookmark = Bookmark.new(comment: Faker::Lorem.paragraph, movie: Movie.all.sample, list: List.all.sample)
+  bookmark.save
+end
+puts "Finished! Created #{Bookmark.count} bookmarks."
+puts "--------------------------------"
+
+puts "Your database is ready !"
+puts "--------------------------------"
